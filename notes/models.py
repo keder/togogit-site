a00f9a02e1d8e9e58aeae21b1af2f4b99af2ff7f
IrinaAg <54929129+IrinaAg@users.noreply.github.com>
2020-10-19 23:01:24 +0300

    Added Repository model and added field in Note

--- notes/models.py
+++ notes/models.py
@@ -8,3 +8,9 @@ class Note(models.Model):
     repo = models.CharField(null=True)
     file_path = models.CharField(null=True)
     line = models.SmallIntegerField(null=True)
+    repository = models.ForeignKey(Repository, on_delete=models.CASCADE)
+
+    
+class Repository(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(null=True)
