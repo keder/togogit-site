770c738f928a04e6103fe3f5f71a8f16c6bd6bce
mary_lev <marylevchenko@gmail.com>
2020-10-24 10:12:16 +0300

    created pages

---
+++ notes/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from . import views
+
+
+app_name = 'notes'
+urlpatterns = [
+	path('', views.index, name='index'),
+	path('repositories', views.RepositoryListView.as_view(), name='repositories'),
+	path('usernotes', views.NoteListView.as_view(), name='notes'),
+	]
